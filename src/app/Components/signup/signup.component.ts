8496a07b976a78b0aa876ba28e6f4bfe9372ff6f
Syed Abdul Basit <79485785+sbasit57@users.noreply.github.com>
2023-05-05 15:07:38 +0500

    5th Commit

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -10,11 +11,16 @@ export class SignupComponent implements OnInit {
   type:string ="password";
 isText: boolean = false;
 eyeIcon: string = "fa-eye-slash";
-  constructor(){
-
-  }
+SignupForm!:FormGroup;
+  constructor(private fb:FormBuilder){}
   ngOnInit(): void {
-
+    this.SignupForm = this.fb.group({
+      firstname: ['',Validators.required],
+      lastname : ['',Validators.required],
+      email    : ['',Validators.required],
+      username : ['',Validators.required],
+      password : ['',Validators.required]
+    })
   }
   hideShowPass(){
     this.isText = !this.isText;
